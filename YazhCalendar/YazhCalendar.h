b2dda1b42a2764267fccadb39eed3c5d4b2d90d9
karuna nithi <karunanithi.ranganadhan-e@maf.ae>
2016-11-16 10:47:15 -0800

    carthage support file commit.

---
+++ YazhCalendar/YazhCalendar.h
@@ -0,0 +1,19 @@
+//
+//  YazhCalendar.h
+//  YazhCalendar
+//
+//  Created by karuna nithi on 11/16/16.
+//  Copyright © 2016 Julien Martin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for YazhCalendar.
+FOUNDATION_EXPORT double YazhCalendarVersionNumber;
+
+//! Project version string for YazhCalendar.
+FOUNDATION_EXPORT const unsigned char YazhCalendarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YazhCalendar/PublicHeader.h>
+
+
